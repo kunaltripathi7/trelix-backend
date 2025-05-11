3812752a309e045a2537d6adf32b40adac1867c8
kunaltripathi7 <ikunaltripathi@gmail.com>
2025-05-11 14:46:17 +0530

    initial commit

---
+++ src/main/java/com/trelix/trelix_app/TrelixAppApplication.java
@@ -0,0 +1,13 @@
+package com.trelix.trelix_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelixAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrelixAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trelix/trelix_app/TrelixAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trelix.trelix_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelixAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
